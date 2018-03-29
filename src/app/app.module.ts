5cc903e38d821412d5fe0ee5cd808186b5c92ef4
Justin Veera <veerakumarcse24@gmail.com>
2018-03-29 22:41:36 +0530

    for raw grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent }  from './app.component';
+import { HttpModule } from '@angular/http';
 
-@NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+@NgModule ({
+   imports:      [ BrowserModule,HttpModule],
+   declarations: [ AppComponent],
+   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
